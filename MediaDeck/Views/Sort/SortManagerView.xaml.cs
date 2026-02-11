032cecb483978cf1285d1a791309eafec2ed4f6a
testakina <testakina@gmail.com>
2026-02-11 15:18:24 +0900

    ソートマネージャーのUI改善

--- MediaDeck/Views/Sort/SortManagerView.xaml.cs
+++ MediaDeck/Views/Sort/SortManagerView.xaml.cs
@@ -1,10 +1,20 @@
 using MediaDeck.Composition.Bases;
+using MediaDeck.Composition.Stores.State.Model.Objects;
 using MediaDeck.ViewModels.Sort;
+
+using Microsoft.UI.Xaml;
 
 namespace MediaDeck.Views.Sort;
 public sealed partial class SortManagerView: SortManagerViewUserControl {
 	public SortManagerView() {
 		this.InitializeComponent();
+	}
+
+	private void RemoveSortItemButton_Click(object sender, RoutedEventArgs e) {
+		if (sender is FrameworkElement { Tag: SortItemObject sortItem } &&
+			this.ViewModel?.CurrentCondition.Value is { } currentCondition) {
+			currentCondition.RemoveSortItemCommand.Execute(sortItem);
+		}
 	}
 }
 
