36b8c3bb00228a708c4e6d464aba3da17dcf317b
xm-i <testakina@gmail.com>
2026-04-12 23:10:13 +0900

    不要ファイル削除

--- MediaDeck/InitialDataRegisterer.cs
+++
@@ -1,9 +0,0 @@
-using MediaDeck.Database;
-
-namespace MediaDeck;
-
-public static class InitialDataRegisterer {
-	public static void Register(MediaDeckDbContext db) {
-		// 初期データの登録が必要な場合はここに記述する
-	}
-}
