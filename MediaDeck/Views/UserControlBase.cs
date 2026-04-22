6b5d2bb2b9325a8a72c1f4ab34805d7f298ad885
xm-i <testakina@gmail.com>
2026-04-23 00:23:48 +0900

    ViewModelをBind

--- MediaDeck/Views/UserControlBase.cs
+++ MediaDeck/Views/UserControlBase.cs
@@ -1,19 +1,49 @@
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
 namespace MediaDeck.Views;
 
+/// <summary>
+/// 指定された型の ViewModel を持つ UserControl の基底クラス。
+/// </summary>
+/// <typeparam name="T">ViewModel の型。</typeparam>
 public abstract class UserControlBase<T> : UserControl where T : class {
+	/// <summary>
+	/// ViewModel 依存関係プロパティを定義します。
+	/// ジェネリッククラス内での定義により、型ごとに個別のプロパティが登録されます。
+	/// </summary>
+	public static readonly DependencyProperty ViewModelProperty =
+		DependencyProperty.Register(
+			nameof(ViewModel),
+			typeof(T),
+			typeof(UserControlBase<T>),
+			new PropertyMetadata(null, OnViewModelPropertyChanged));
+
+	/// <summary>
+	/// ViewModel を取得または設定します。
+	/// </summary>
 	public T? ViewModel {
-		get;
-		set;
+		get {
+			return (T?)this.GetValue(ViewModelProperty);
+		}
+
+		set {
+			this.SetValue(ViewModelProperty, value);
+		}
 	}
 
-	protected UserControlBase() {
-		this.DataContextChanged += (_, _) => {
-			var old = this.ViewModel;
-			this.ViewModel = this.DataContext as T;
-			this.OnViewModelChanged(old, this.ViewModel);
-		};
+	private static void OnViewModelPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+		if (d is UserControlBase<T> control) {
+			var newValue = e.NewValue as T;
+
+			// DataContext を同期する
+			if (control.DataContext != newValue) {
+				control.DataContext = newValue;
+			}
+
+			// 仮想メソッドを呼び出す
+			control.OnViewModelChanged(e.OldValue as T, newValue);
+		}
 	}
 
 	/// <summary>
